96d9dfe24e36665316a72f0344baab0b16f53430
0xCF8 <zzje2077@gmail.com>
2026-05-03 22:18:56 -0500

    Fix static export build errors and ensure dynamic params support

--- apps/web-app/app/servers/[id]/channels/[cid]/page.tsx
+++ apps/web-app/app/servers/[id]/channels/[cid]/page.tsx
@@ -1,16 +1,16 @@
 import ChannelView from '@/app/components/ChannelView';
 
-export default async function ChannelPage({
-  params,
-}: {
+type Props = {
   params: Promise<{ id: string; cid: string }>;
-}) {
+};
+
+export default async function ChannelPage({ params }: Props) {
   const { id, cid } = await params;
   return <ChannelView serverId={id} channelId={cid} />;
 }
 
-
-
 export function generateStaticParams() {
-  return [{ id: "default", cid: "default" }];
+  return [{ id: 'default', cid: 'default' }];
 }
+
+export const dynamicParams = true;
